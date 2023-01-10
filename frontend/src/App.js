fdf34b6f9c0e47b2116450ba5ba8872917e87f51
Richard Kruse <richardkruse3rd@gmail.com>
2023-01-10 22:34:04 +0900

    downloaded &installed react-bootsrap, custom theme, & font-awesome then bult navbar & footer components with em

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import { Container } from 'react-bootstrap'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -6,7 +8,9 @@ function App() {
     <div >
       <Header />
       <main>
+      <Container className='py-3'>
         <h1>Welcome</h1>
+      </Container>
       </main>
       <Footer />
     </div>
