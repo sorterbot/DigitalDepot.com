71433eacbacde7cb1d5cad02024c2ed8113e3367
Richard Kruse <richardkruse3rd@gmail.com>
2023-01-13 14:04:40 +0900

    created serializers class, removed static products.py files, & conneceted backend to tru sql light database

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import *
 from .products import products
+from .serializers import ProductSerializer
 # Create your views here.
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def getRoutes(request):
@@ -24,14 +27,18 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
     
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i["_id"] == pk:
-            product = i
-            break
+    # product = None
+    # for i in products:
+    #     if i["_id"] == pk:
+    #         product = i
+    #         break
 
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
