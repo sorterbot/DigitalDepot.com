2e9946c250367cf1e817ea1a7df185046c46d237
Richard Kruse <richardkruse3rd@gmail.com>
2023-01-15 12:23:40 +0900

    created message & loader components and plugged them into hoemscreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,33 +1,40 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+// import Paginate from '../components/Paginate'
+// import ProductCarousel from '../components/ProductCarousel'
+import { listProducts } from '../actions/productActions'
 
 
 
 function HomeScreen() {
-  const [products, setProducts ] = useState([])
+
+  const dispatch = useDispatch()
+  const productList = useSelector(state => state.productList)
+  const {error, loading, products} = productList
 
   useEffect(() => {
-    async function fetchProducts() {
-      const {data} = await axios.get('http://127.0.0.1:8000/api/products/')
-      setProducts(data)
-    }
+    dispatch(listProducts())
+  }, [dispatch])
 
-    fetchProducts()
-
-  }, [])
   
   return (
     <div>
         <h1>Latest Products</h1>
-        <Row>
+        {loading ? <Loader />
+          : error ? <Message variant='danger'>{error}</Message>
+          : 
+          <Row>
             {products.map(product => (
                 <Col key={product._id} sm={12} md={6} lg={4} xL={3}>
-                   <Product product={product} />
+                  <Product product={product} />
                 </Col>
             ))}
-        </Row>
+          </Row>
+        }       
     </div>
   )
 }
