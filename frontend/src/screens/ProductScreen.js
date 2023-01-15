6318350e09bb3d44de3e7addfc9c723e0fe32179
Richard Kruse <richardkruse3rd@gmail.com>
2023-01-15 11:53:30 +0900

    added action & constant & reducers for product list, stage one+ of rolling through the redux kit

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Button, Card, } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import axios from 'axios'
 
