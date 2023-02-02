09daccd56bf776b6c9ca53938c02563658e00530
Richard Kruse <richardkruse3rd@gmail.com>
2023-02-02 15:22:26 +0900

    replaced users view taking info from static list to a dynamic users info grab from database & added a layer of only admin's permissions to view the page. Added auth requirements for several other pages & tested with postman

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('users/login/', views.TokenObtainPairView.as_view(), 
+    path('users/login/', views.MyTokenObtainPairView.as_view(), 
     name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('users/profile/', views.getUserProfile, name="user-profile"),
+    path('users/', views.getUsers, name="users"),
     path('products/', views.getProducts, name="products"),
     path('product/<str:pk>/', views.getProduct, name="product"),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
+from django.contrib.auth.models import User
 from .models import *
 from .products import products
-from .serializers import ProductSerializer, UserSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -15,8 +17,10 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
 
-        data['username'] = self.user.username
-        data['email'] = self.user.email 
+        serializer = UserSerializerWithToken(self.user).data
+
+        for k, v in serializer.items():
+            data[k] = v
 
         return data
 
@@ -44,9 +48,17 @@ def getRoutes(request):
     return Response(routes, safe=False)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 
