8b1fd5add87b46bac3242dd6183a03d201b26def
Richard Kruse <richardkruse3rd@gmail.com>
2023-02-04 18:41:09 +0900

    P49 added payment screen & functions

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -3,7 +3,7 @@ import {
     CART_REMOVE_ITEM,
     CART_SAVE_SHIPPING_ADDRESS,
 
-    // CART_SAVE_PAYMENT_METHOD,
+    CART_SAVE_PAYMENT_METHOD,
 
     // CART_CLEAR_ITEMS,
 } from '../constants/cartConstants'
@@ -40,11 +40,11 @@ export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, acti
                 shippingAddress: action.payload
             }
 
-        // case CART_SAVE_PAYMENT_METHOD:
-        //     return {
-        //         ...state,
-        //         paymentMethod: action.payload
-        //     }
+        case CART_SAVE_PAYMENT_METHOD:
+            return {
+                ...state,
+                paymentMethod: action.payload
+            }
 
         // case CART_CLEAR_ITEMS:
         //     return {
