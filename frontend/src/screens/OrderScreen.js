356004109511edb124f172e4666f38eb1ccdbf6e
Richard Kruse <richardkruse3rd@gmail.com>
2023-02-05 22:18:49 +0900

    Added Paypal integratoin. May upgrade certain features later according to current API features

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,19 +1,31 @@
-import React, { useStatte, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
+import { PayPalButton } from 'react-paypal-button-v2'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import CheckoutSteps from '../components/CheckoutSteps'
-import { getOrderDetails } from '../actions/orderActions'
+import { getOrderDetails, payOrder, deliverOrder } from '../actions/orderActions'
+import { ORDER_PAY_RESET, ORDER_DELIVER_RESET } from '../constants/orderConstants'
 
 function OrderScreen({ match }) {
 
     const orderId = match.params.id
     const dispatch = useDispatch()
 
+    const [sdkReady, setSdkReady] = useState(false)
+
     const orderDetails = useSelector(state => state.orderDetails)
     const { order, error, loading } = orderDetails
+
+    const orderPay = useSelector(state => state.orderPay)
+    const { loading: loadingPay, success: successPay } = orderPay
+
+    const orderDeliver = useSelector(state => state.orderDeliver)
+    const { loading: loadingDeliver, success: successDeliver } = orderDeliver
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
 
 
     const cart = useSelector(state => state.cart)
@@ -22,15 +34,44 @@ function OrderScreen({ match }) {
         order.itemsPrice = order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0).toFixed(2)
     }
 
-
+    const addPayPalScript = () => {
+        const script = document.createElement('script')
+        script.type = 'text/javascript'
+        script.src = 'https://www.paypal.com/sdk/js?client-id=AbmRz9HzYCqIbukwxIJbwh-y6xbpNpKZbAXHTt7pu92qiiuJkqUvMks1rXl-EMhJo1k4V0EPsxyVwOtm'
+        script.onload = () => {
+            setSdkReady(true)
+        }
+        document.body.appendChild(script)
+    }
 
     useEffect(() => {
-        if( !order || order._id !== Number(orderId)){
-            dispatch(getOrderDetails(orderId))
 
+        // if (!userInfo) {
+        //     history.push('/login')
+        // }
+
+        if (!order || successPay || order._id !== Number(orderId) || successDeliver) {
+            dispatch({ type: ORDER_PAY_RESET })
+            dispatch({ type: ORDER_DELIVER_RESET })
+
+            dispatch(getOrderDetails(orderId))
+        } else if (!order.isPaid) {
+            if (!window.paypal) {
+                addPayPalScript()
+            } else {
+                setSdkReady(true)
+            }
         }
- 
-    }, [order, orderId])
+    }, [dispatch, order, orderId, successPay, successDeliver])
+
+
+    const successPaymentHandler = (paymentResult) => {
+        dispatch(payOrder(orderId, paymentResult))
+    }
+
+    // const deliverHandler = () => {
+    //     dispatch(deliverOrder(order))
+    // }
 
 
     return loading ? (
@@ -141,6 +182,22 @@ function OrderScreen({ match }) {
                                     <Col>${order.totalPrice}</Col>
                                 </Row>
                             </ListGroup.Item>
+
+
+                            {!order.isPaid && (
+                                <ListGroup.Item>
+                                    {loadingPay && <Loader />}
+
+                                    {!sdkReady ? (
+                                        <Loader />
+                                    ) : (
+                                            <PayPalButton
+                                                amount={order.totalPrice}
+                                                onSuccess={successPaymentHandler}
+                                            />
+                                        )}
+                                </ListGroup.Item>
+                            )}
 
                         </ListGroup>
                     </Card>
