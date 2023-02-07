0ab12df0090b6dc5aecc6b814249cfcef05be658
Richard Kruse <richardkruse3rd@gmail.com>
2023-02-07 21:21:15 +0900

    made backend API support func for product delete/create/update

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -5,8 +5,12 @@ urlpatterns = [
 
     path('', views.getProducts, name="products"),
 
+    path('create/', views.createProduct, name="product-create"),
+
    
     path('<str:pk>/', views.getProduct, name="product"),
 
-  
+    path('update/<str:pk>/', views.updateProduct, name="product-update"),
+    path('delete/<str:pk>/', views.deleteProduct, name="product-delete"),
+
 ]
