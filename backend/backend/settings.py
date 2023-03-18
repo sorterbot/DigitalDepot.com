dc5eda9639a22f8caaa5fec3c52b23d48d02d728
Richard Kruse <richardkruse3rd@gmail.com>
2023-03-18 20:53:53 +0900

    most of the way through configuring the site between itself, AWS, PA admin, and btbd if for heroku or the other one.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os 
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -25,7 +27,7 @@ SECRET_KEY = 'x+!uq8hww)bhqq9qykbq_jm*tp0!twqxmc&=6ij(9x3ip5j9)!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     
     'rest_framework',
     'corsheaders',
+    'storages',
 
     'base.apps.BaseConfig',
 ]
@@ -82,6 +85,7 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -97,7 +101,9 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +124,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'digitaldepot',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres0327',
+        'HOST': 'digitaldepot-identifier.cly4jz27rb7g.us-west-2.rds.amazonaws.com',
+        'PORT':'5432', 
     }
 }
 
@@ -164,9 +174,21 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static' ,
+    BASE_DIR / 'frontend/build/static'
 ]
 
-MEDIA_ROOT = 'static/images'
+MEDIA_ROOT = BASE_DIR /'static/images'
+STATIC_ROOT = BASE_DIR /'staticfiles'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+AWS_QUERYSTRING_AUTH = False
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_ACCESS_KEY_ID = 'AKIARDPX5CQUVLNXLQV3'
+AWS_S3_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'digitaldepot-bucket'
+
+if os.getcwd() == '/app':
+    DEBUG = False 
